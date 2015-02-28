c554f2e08c49701b18c0105a11121d09193b878f
DarkoreXOR <DarkoreXOR@gmail.com>
2015-03-01 02:02:58 +0600

    Решение задачи и добавлено несколько тестов

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -1,8 +1,52 @@
 package psu.kit.tanya;
 
+import java.util.HashMap;
+
 public class TanyaCalculator {
 
+    private HashMap<Character, Integer> map;
+
+    private void decreaseValue(char key) {
+        map.put(key, map.get(key) - 1);
+    }
+
+    private void increaseValue(char key) {
+        map.put(key, map.getOrDefault(key, 0) + 1);
+    }
+
+    private Integer getValue(char key) {
+        return map.getOrDefault(key, 0);
+    }
+
     public TanyaReaction calculate(String s, String t) {
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+        map = new HashMap<>();
+
+        int yayCount = 0;
+        int whoopsCount = 0;
+
+        for (int i = 0; i < t.length(); ++i) {
+            char ch = t.charAt(i);
+            increaseValue(ch);
+        }
+
+        StringBuilder text = new StringBuilder(s);
+        for (int i = 0; i < text.length(); ++i) {
+            char ch = text.charAt(i);
+            if (getValue(ch) > 0) {
+                decreaseValue(ch);
+                text.setCharAt(i, '\0');
+                ++yayCount;
+            }
+        }
+
+        for (int i = 0; i < text.length(); ++i) {
+            char ch = text.charAt(i);
+            ch = Character.isUpperCase(ch) ? Character.toLowerCase(ch) : Character.toUpperCase(ch);
+            if (getValue(ch) > 0) {
+                decreaseValue(ch);
+                ++whoopsCount;
+            }
+        }
+        return new TanyaReaction(yayCount, whoopsCount);
     }
 }
--- test/psu/kit/tanya/TanyaCalculatorTest.java
+++ test/psu/kit/tanya/TanyaCalculatorTest.java
@@ -9,18 +9,28 @@ import static org.testng.Assert.*;
 public class TanyaCalculatorTest {
     private TanyaCalculator calc;
 
-
     @BeforeMethod
     public void setUp() throws Exception {
         calc = new TanyaCalculator();
     }
 
     @DataProvider(name = "Tanya")
-    public Object[][] getTanya(){
-        return new Object[][]{
-                {"AbC", "DCbA", 3, 0}
+    public Object[][] getTanya() {
+        return new Object[][] {
+                // codeforces tests
+                {"AbC", "DCbA", 3, 0},
+                {"ABC", "abc", 0, 3},
+                {"abacaba", "AbaCaBA", 3, 4},
+                {"zzzzz", "ZZZZZ", 0, 5},
+                {"zzzZZZ", "ZZZzzZ", 5, 1},
+                {"abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 26},
+                {"l", "FPbAVjsMpPDTLkfwNYFmBDHPTDSWSOUlrBHYJHPM", 1, 0},
+                {"ncMeXssLHS", "uwyeMcaFatpInZVdEYpwJQSnVxLK", 6, 1},
+                {"DpiNBmCRFWxpdbfGOzvvOcemjructoAdEwegTvbVbfWWRPGyEAxGdDRWVlqNyGWMWHMrHAIZpyxvgaflrsVZhhZRouvpxrKXFZam",
+                        "wwPLFtNfPtJXvMLuHjKfYyaRhreNSWSzOvDpqHCGcqllACNPGHxReeFUCmAqIKXYytsSQwIxJzNiiUtgebVuwRmWpRALLyKAzyDPvgIGxALSaeeTIqm\n", 66, 12},
+                {"sabc", "AbCsS", 2, 2},
+                {"x", "xX", 1, 0}
         };
-
     }
 
     @Test(dataProvider = "Tanya")
