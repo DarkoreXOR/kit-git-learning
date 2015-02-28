0a5ab7bef6ce102fe960e76a15de69bdd6a4a712
Dmitry Pobegaev <demonnotdead@mail.ru>
2015-02-28 10:08:24 +0500

    little fix

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -2,7 +2,7 @@ package psu.kit.tanya;
 
 public class TanyaCalculator {
 
-    public int calculate(String s, String t) {
+    public TanyaReaction calculate(String s, String t) {
         throw new UnsupportedOperationException("Метод ещё не реалиозован");
     }
 }
